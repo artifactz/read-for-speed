13063a9f64de21108ef53a7b6907408b8ac6acdd
Jan Brehmer <brehmeja+git@gmail.com>
2025-05-17 20:21:33 +0200

    :sparkles: added confusion matrix to eval

--- ml/font/evaluation.py
+++ ml/font/evaluation.py
@@ -1,6 +1,7 @@
-import sys, os, collections, logging, multiprocessing, datetime, json
+import sys, os, collections, logging, multiprocessing, datetime, json, itertools
 from pathlib import Path
 import numpy as np
+import matplotlib.pyplot as plt
 from tqdm import tqdm
 from ml.font import extract
 import util.tile_image
@@ -12,8 +13,8 @@ MODEL_TYPE = "ONNX"  # or "TORCH"
 
 
 def run_crop_data_eval(model, batch_size: int = 1024):
-    """Runs an evaluation of the model on the evaluation_data folder."""
-    crops, labels = load_crop_data()
+    """Runs a deterministic evaluation of the model on the evaluation_data folder."""
+    crops, labels = _load_crop_data()
     output_chunks = []
     for i in tqdm(range(0, len(crops), batch_size), desc="Evaluating batches"):
         start = i
@@ -21,45 +22,47 @@ def run_crop_data_eval(model, batch_size: int = 1024):
         batch = crops[start:end]
         output_chunks.append(model.predict(batch))
     outputs = np.concatenate(output_chunks, axis=0)
-    hits, misses = verify_outputs(outputs, model.classes, labels)
+    predicted_class_ids = outputs.argmax(axis=1)
+    label_class_ids = _classes_to_ids(labels, model.classes)
 
-    stats = calculate_stats(hits, misses)
-    for c, acc in stats["class_accs"].items():
-        print(f"{c} accuracy: {acc * 100:.2f}%")
-    print(f"Total accuracy: {stats['total_accuracy'] * 100:.2f}%")
-    print(f"Mean accuracy: {stats['mean_acc'] * 100:.2f}%")
+    _run_common_eval(predicted_class_ids, label_class_ids, model.classes)
 
 
-def load_crop_data():
-    """Reads images and labels from the evaluation_data folder."""
-    samples = []
-    for path, folders, files in Path(EVALUATION_DATA_PATH).walk():
-        for file in files:
-            if not file.endswith(".png"):
-                continue
-            for img in util.tile_image.read_tile_image(path / file, 128):
-                label = path.parent.stem
-                samples.append((img, label))
+def run_pdf_files_eval(num_samples: int):
+    """Runs a non-deterministic evaluation of the model on the pdf files defined in `EVALUATION_PDFS_PATH`."""
+    pairs = _get_pdf_labels_predictions(EVALUATION_PDFS_PATH, num_samples)
 
-    return zip(*samples)
+    predictions, labels = zip(*pairs)
+    classes = sorted(set(predictions) | set(labels))
+    predicted_class_ids = _classes_to_ids(predictions, classes)
+    label_class_ids = _classes_to_ids(labels, classes)
+
+    _run_common_eval(predicted_class_ids, label_class_ids, classes)
 
 
-def verify_outputs(outputs, classes, labels):
-    """Compares the model outputs with the labels and counts hits and misses."""
+def _run_common_eval(predicted_class_ids, label_class_ids, classes):
+    hits, misses = _verify_predictions(predicted_class_ids, label_class_ids, classes)
+    stats = _calculate_stats(hits, misses)
+    _print_stats(stats)
+
+    confusion_matrix = _get_confusion_matrix(predicted_class_ids, label_class_ids)
+    _plot_confusion_matrix(confusion_matrix, classes)
+
+
+def _verify_predictions(predicted_class_ids, label_class_ids, classes):
+    """Compares the predicted classes with the labels and counts hits and misses per class."""
     hits, misses = collections.Counter(), collections.Counter()
 
-    for sample_outputs, label in zip(outputs, labels):
-        predicted = sample_outputs.argmax()
-        predicted = classes[predicted]
-        if predicted == label:
-            hits[label] += 1
+    for predicted_class_id, label_class_id in zip(predicted_class_ids, label_class_ids):
+        if predicted_class_id == label_class_id:
+            hits[classes[label_class_id]] += 1
         else:
-            misses[label] += 1
+            misses[classes[label_class_id]] += 1
 
     return hits, misses
 
 
-def calculate_stats(hits, misses):
+def _calculate_stats(hits, misses):
     classes = set(hits.keys()) | set(misses.keys())
     class_accs = {c: (hits[c] / (hits[c] + misses[c])) if hits[c] + misses[c] > 0 else None for c in classes}
 
@@ -79,14 +82,28 @@ def calculate_stats(hits, misses):
     }
 
 
-def run_pdf_files_eval(num_samples: int):
-    """Runs an evaluation of the model on the pdf files defined in `EVALUATION_PDFS_PATH`."""
-    hits, misses = verify_pdf_files(EVALUATION_PDFS_PATH, num_samples)
-    total = hits + misses
-    print(f"Total accuracy ({num_samples} samples): {hits / total * 100:.2f}%  (correct: {hits}, incorrect: {misses})")
+def _print_stats(stats):
+    for c, acc in stats["class_accs"].items():
+        print(f"{c} accuracy: {acc * 100:.2f}%")
+    print(f"Total accuracy: {stats['total_accuracy'] * 100:.2f}%")
+    print(f"Mean accuracy: {stats['mean_acc'] * 100:.2f}%")
 
 
-def verify_pdf_files(pdfs_file: str | Path, num_samples: int):
+def _load_crop_data():
+    """Reads images and labels from the evaluation_data folder."""
+    samples = []
+    for path, folders, files in Path(EVALUATION_DATA_PATH).walk():
+        for file in files:
+            if not file.endswith(".png"):
+                continue
+            for img in util.tile_image.read_tile_image(path / file, 128):
+                label = path.parent.stem
+                samples.append((img, label))
+
+    return zip(*samples)
+
+
+def _get_pdf_labels_predictions(pdfs_file: str | Path, num_samples: int) -> list[tuple[str, str]]:
     logging.getLogger('pdfminer.pdfpage').setLevel(logging.ERROR)
 
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
@@ -95,20 +112,13 @@ def verify_pdf_files(pdfs_file: str | Path, num_samples: int):
     with open(pdfs_file) as f:
         paths = json.load(f)
 
-    hits, misses = 0, 0
     args = [(path, num_samples, out_folder) for path in paths]
     pool = multiprocessing.Pool(14, initializer=_initialize_pool_process)
     results = list(tqdm(pool.imap(_get_pdf_label_prediction_star, args), total=len(args), desc="Evaluating PDF files"))
     pool.close()
     pool.join()
 
-    for label, prediction in results:
-        if prediction == label:
-            hits += 1
-        else:
-            misses += 1
-
-    return hits, misses
+    return results
 
 
 def _get_pdf_label_prediction_star(args):
@@ -157,6 +167,49 @@ def _load_model(model_path: str = "ml/font/model.onnx", classes_path: str = "ml/
         return estimator_onnx.Model(model_path, classes_path)
     else:
         raise ValueError(f"Unknown model type: {MODEL_TYPE}")
+
+
+def _classes_to_ids(class_name_list, class_names):
+    return [class_names.index(class_name) for class_name in class_name_list]
+
+
+def _get_confusion_matrix(predicted_class_ids, label_class_ids):
+    ids = set(predicted_class_ids) | set(label_class_ids)
+    n = len(ids)
+    matrix = np.zeros((n, n), int)
+    for pred, label in zip(predicted_class_ids, label_class_ids):
+        matrix[label, pred] += 1
+    return matrix
+
+
+def _plot_confusion_matrix(matrix: np.ndarray, classes=None, normalize_rows=True):
+    n = len(classes)
+    if normalize_rows:
+        matrix = np.array(matrix, dtype=float)
+        for i in range(matrix.shape[0]):
+            matrix[i] /= matrix[i].sum()
+
+    plt.imshow(matrix, "Oranges")
+
+    # Gridlines based on minor ticks, have to draw them thick because they may appear inaccurate at some scales
+    plt.xticks(np.arange(-.5, n, 1), minor=True)
+    plt.yticks(np.arange(-.5, n, 1), minor=True)
+    plt.grid(which='minor', color='lightgray', linestyle='-', linewidth=2)
+
+    for label, pred in itertools.product(range(n), range(n)):
+        ratio = matrix[label, pred] / np.sum(matrix[label])
+        if ratio >= 0.01:
+            ratio_str = f"{ratio * 100:.3g}%"
+            plt.text(pred, label, ratio_str, size="smaller", horizontalalignment="center", verticalalignment="center")
+
+    plt.title("Confusion matrix")
+    plt.xlabel("Prediction")
+    plt.ylabel("Truth")
+    if classes:
+        plt.xticks(range(n), labels=classes, rotation=33, horizontalalignment="right")
+        plt.yticks(range(n), labels=classes)
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == "__main__":
